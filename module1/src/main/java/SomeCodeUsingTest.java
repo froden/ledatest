059b2fe6cfa9b38e70318360340bd507d9a2e06c
Frode Nerbråten <frode@nerbraten.no>
2012-09-07 10:05:48 +0200

    Initial

---
+++ module1/src/main/java/SomeCodeUsingTest.java
@@ -0,0 +1,5 @@
+public class SomeCodeUsingTest {
+    public static void main(String[] args) {
+        System.out.println(new SomeTest().echo("Hello world"));
+    }
+}
---
+++ module2/src/test/java/SomeTest.java
@@ -0,0 +1,5 @@
+public class SomeTest {
+    public String echo(String str) {
+        return str;
+    }
+}
